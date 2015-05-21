521bfee63ca3a749a0801c8a5b92157da72e4b8a
michals92 <michal.simik@gmail.com>
2015-05-21 22:53:04 +0200

    UI changes

---
+++ CloudAlarm/IntroHelloRegisterViewController.swift
@@ -0,0 +1,38 @@
+//
+//  IntroHelloRegisterViewController.swift
+//  CloudAlarm
+//
+//  Created by Michal Šimík on 21.05.15.
+//  Copyright (c) 2015 cz.fi.muni. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class IntroHelloRegisterViewController: UIView {
+
+    @IBInspectable var startColor: UIColor = UIColor.redColor()
+    @IBInspectable var endColor: UIColor = UIColor.greenColor()
+    
+    override func drawRect(rect: CGRect) {
+        
+        let context = UIGraphicsGetCurrentContext()
+        let colors = [startColor.CGColor, endColor.CGColor]
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let colorLocations:[CGFloat] = [0.0, 1.0]
+        
+        let gradient = CGGradientCreateWithColors(colorSpace,
+            colors,
+            colorLocations)
+        
+        var startPoint = CGPoint.zeroPoint
+        var endPoint = CGPoint(x:0, y:self.bounds.height)
+        CGContextDrawLinearGradient(context,
+            gradient,
+            startPoint,
+            endPoint,
+            0)
+    }
+
+}
